bc30a320aec7f765037d9da3832db4c34b567caa
artemPGH <facts.com99@gmail.com>
2026-01-20 19:11:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // --- НАСТРОЙКИ ---
 const SUPABASE_URL = "https://ptetkaidxtignrlhrbpj.supabase.co";
-const SUPABASE_KEY = "sb_publishable_Y5HdMr6bd9FZKJXk-bK0vw_JPVOLigb";
+const SUPABASE_KEY = "sb_publishable_Y5HdMr6bD9FZKJXk-bK0vw_JPV0Ligb";
 const SEARCH_API = "https://artix-search.facts-com99.workers.dev/api/search";
 
 const { createClient } = window.supabase;
